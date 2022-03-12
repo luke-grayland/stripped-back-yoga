19fffd4548b5f82a5c697872d7ced8ba3595b42d
Luke Grayland <lgrayland96@gmail.com>
2022-03-12 23:21:49 +0000

    nav bar started

---
+++ src/Nav/Nav.js
@@ -0,0 +1,24 @@
+const Nav = () => {
+    return (
+        <>
+            <div className="navBar">
+                <div className="navBarContent">
+                    <div className="navBarLogoContainer">
+                        <div className="navLogoImg">
+                        </div>
+                        <div className="navLogoText">
+                        </div>
+                    </div>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Me</li>
+                        <li>Classes</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Nav
